bc54930b23bbbe1b7d94c9434fbc74b97b77aaf5
nabilafandi <nabilafandih@gmail.com>
2023-06-06 14:23:37 +0700

    router test

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 import { ThemeProvider } from "@mui/material";
-import { Routes, Route } from "react-router-dom";
-import { Container } from "@mui/material";
 import Theme from "./Theme";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import SideMenu from "./components/Sidemenu";
-import Topmenu from "./components/Topmenu";
-
+import Root from "./pages/Root";
+// import { Container } from "@mui/material";
+// import SideMenu from "./components/Sidemenu";
+// import Topmenu from "./components/Topmenu";
 import Payment from "./pages/Payment";
 import User from "./pages/User";
 import Support from "./pages/Support";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/Payment" element={<Payment />} />
+      <Route path="/User" element={<User />} />
+      <Route path="/Support" element={<Support />} />
+    </Route>
+  ) 
+);
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={Theme}>
-        <Topmenu />
-
-        <div style={{ display: "flex", width: "100%" }}>
-          <SideMenu />
-          <div style={{ width: "100%" }}>
-            <div
-              style={{
-                height: "100px",
-              }}
-            ></div>
-            <main>
-              <Container sx={{}}>
-                <Routes>
-                  <Route path="payment" element={<Payment />} />
-                  <Route path="user" element={<User />} />
-                  <Route path="Support" element={<Support />} />
-                </Routes>
-              </Container>
-            </main>
-          </div>
-        </div>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </div>
   );
